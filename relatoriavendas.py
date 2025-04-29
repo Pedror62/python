943043a86f90220fd355aff0b628d302aa54d9fd
pedror62 <102195011+Pedror62@users.noreply.github.com>
2025-04-29 19:59:27 -0300

    adicionado finalizacao

--- relatoriavendas.py
+++ relatoriavendas.py
@@ -1,5 +1,6 @@
 # 1º pecorrer todas as pastar de vendas 
 import os
+from sys import displayhook
 import pandas as pd 
 
 lista_arquivos = os.listdir("Vendas")
@@ -12,14 +13,22 @@ tabela_total = pd.DataFrame()
 for arquivos in lista_arquivos:
        
     if "Vendas" in arquivos:
+        print()
+        print('')
+        #importar arquivo
         tabela = pd.read_csv(f'Vendas/{arquivos}')
+        # print(arquivos)
+        # displayhook(tabela)
         tabela_total = pd.concat([tabela_total, tabela], ignore_index=True)
-        
-        
+displayhook(tabela_total)
+print(arquivos)
+displayhook(tabela)
 
 #3º tratar ou fazer a compilação 
-
 #4º realizar calculo de produto mais vendido 
+tabela_prduto = tabela_total.groupby('Produto').sum()
+tabela_prduto = tabela_prduto[["Quantidade Vendida" , "Primeiro Nome"]]
+displayhook(tabela_prduto)
 
 #5º produto que mais faturou
 
